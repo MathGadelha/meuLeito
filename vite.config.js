6e4084d40b924f949112f9a351833e6b6a84abe6
Matheus Gadelha <matheus.farias@icc.org.br>
2025-10-06 07:55:34 -0300

    feat(gitignore): add gitignore

--- vite.config.js
+++ vite.config.js
@@ -5,16 +5,15 @@ import { defineConfig } from "vite";
 import tsConfigPaths from "vite-tsconfig-paths";
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react(), tsConfigPaths()],
-    base: "/meuLeito",
-    resolve: {
-        alias: {
-            "@": path.resolve(__dirname, "./src"),
-        },
-    },
-    test: {
-        globals: true,
-        environment: "jsdom",
-        setupFiles: ['./vitest.setup.ts'],
-    },
+	plugins: [react(), tsConfigPaths()],
+	resolve: {
+		alias: {
+			"@": path.resolve(__dirname, "./src"),
+		},
+	},
+	test: {
+		globals: true,
+		environment: "jsdom",
+		setupFiles: ["./vitest.setup.ts"],
+	},
 });
